8f4d0c2f76cbc028e834cfe17337427eef04f20b
SlavicTofu <80066554+SlavicTofu@users.noreply.github.com>
2026-02-05 22:18:12 -0500

    Begun Pong
    started work on pong game, making sure to keep game-code and 3DS-specific code separate, to allow for it to be run on 3DS or using other graphics libraries

---
+++ pong/source/graphics.cpp
@@ -0,0 +1,5 @@
+/* 
+ * Contains helper functions which will work to abstract the game code
+ * from the 3DS-specific graphics code, which will make it easier to
+ * build the project for other devices.
+ */
---
+++ pong/source/pong.cpp
@@ -0,0 +1,103 @@
+enum GAME_STATE
+{
+	MENU,
+	IN_GAME,
+	EXIT
+};
+
+enum PLAY_STATE
+{
+	SERVE_PLAYER_ONE,
+	SERVE_PLAYER_TWO,
+	TOWARD_PLAYER_ONE,
+	TOWARD_PLAYER_TWO
+};
+
+class Vector2D
+{
+public:
+	float x;
+	float y;
+};
+
+class Rectangle
+{
+public:
+	float x;
+	float y;
+	float width;
+	float height;
+};
+
+class Court
+{
+private:
+	Rectangle dimensions;
+public:
+	Court(Rectangle dims)
+	{
+		dimensions = dims;
+	}
+	Rectangle getDimensions()
+	{
+		return dimensions;
+	}
+};
+
+class Paddle
+{
+private:
+	Rectangle m_rect;
+public:
+	Paddle(Rectangle rect)
+	{
+		m_rect = rect;
+	}
+	Rectangle getPositionSize()
+	{
+		return m_rect;
+	}
+	void setPositionSize(Rectangle rect)
+	{
+		m_rect = rect;
+	}
+	void setPosition(Vector2D newPos)
+	{
+		m_rect.x = newPos.x;
+		m_rect.y = newPos.y;
+	}
+};
+
+class Ball
+{
+private:
+	Vector2D m_position;
+	float m_radius;
+	Vector2D m_velocity;
+public:
+	Ball(Vector2D position, float radius)
+	{
+		m_position = position;
+		m_radius = radius;
+	}
+	Vector2D getPosition()
+	{
+		return m_position;
+	}
+	float getRadius()
+	{
+		return m_radius;
+	}
+	Vector2D getVelocity()
+	{
+		return m_velocity;
+	}
+	void setPosition(Vector2D position)
+	{
+		m_position = position;
+	}
+	void setVelocity(Vector2D velocity)
+	{
+		m_velocity = velocity;
+	}
+};
